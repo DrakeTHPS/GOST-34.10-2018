a60d6dfcaf75b10e11dbab8ae5a28179882b4b78
DrakeTHPS <48874682+DrakeTHPS@users.noreply.github.com>
2020-04-23 20:20:03 +0500

    Add files via upload

---
+++ Stribog256.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalSignature
+{
+    class Stribog256 : Stribog
+    {
+        public override byte[] GetHash(string message)
+        {
+            N = new byte[64];
+            Sigma = new byte[64];
+            for (int i = 0; i < 64; i++)
+            {
+                iv[i] = 0x01;
+            }
+            byte[] hash = GetHashX(Encoding.ASCII.GetBytes(message));
+            byte[] h256 = new byte[32];
+            Array.Copy(hash, 0, h256, 0, 32);
+            return h256;
+        }
+
+    }
+}
